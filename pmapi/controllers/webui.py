a098a4b40b5c8ec19197dd49af7352c30dfd83e5
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-01 08:06:36 -0600

    things

---
+++ pmapi/controllers/webui.py
@@ -0,0 +1,34 @@
+import logging
+
+from pmapi.config import get_config
+from pmapi.services.sync import sync_directory
+
+c = get_config()
+
+
+def post_deploy_webui(data):
+    """
+    Post to deploy webui updates
+    :param data:
+    :return:
+    """
+    try:
+        logging.info("Updating webui directory {} to remote {}".format(data["local"], data["remote"]))
+        sync = sync_directory(data["remote"], data["local"], data["remote_host"], data["remote_user"], data["commands"])
+        if sync:
+            logging.info("successfully synced {} directory to {}".format(data["local"], data["remote_host"]))
+            response = {
+                "status": "success",
+                "message": "successfully synced {} directory to {}".format(data["local"], data["remote_host"])
+            }
+            return response, 200
+        else:
+            logging.info("failed to sync {} directory to {}".format(data["local"], data["remote_host"]))
+            response = {
+                "status": "failure",
+                "message": "failed to sync {} directory to {}".format(data["local"], data["remote_host"])
+            }
+            return response, 409
+    except Exception as e:
+        logging.error("failed to sync directory {}: {}".format(data["local"], e))
+        raise e
--- pmapi/services/sync.py
+++ pmapi/services/sync.py
@@ -46,3 +46,31 @@ def sync_repo(repo):
     except Exception as e:
         logging.error("Failed to sync repo {}: {}".format(repo, e))
         raise e
+
+
+def sync_directory(remote, local, remote_host, remote_user, commands=None):
+    try:
+        logging.info("syncing directory {} to {}".format(local, remote))
+        rsyncopts = "-rlptgoH --delay-updates --stats"
+        remotersyncopt = "-e 'ssh -i /home/mirroradmin/.ssh/id_rsa'"
+
+        cmd = "/usr/bin/rsync --delete {} {} {} {}@{} {}".format(rsyncopts, remotersyncopt, local, remote_user,
+                                                                 remote_host, remote)
+        stat = os.system(cmd)
+        if stat != 0:
+            raise Exception("Error with rsync.")
+
+        if commands:
+            private_key = paramiko.RSAKey.from_private_key_file(c["MIRROR_ADMIN_KEY"])
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.connect(remote_host, username=remote_user, pkey=private_key)
+            for cmd in commands:
+                stdin, stdout, stderr = client.exec_command(cmd)
+                for line in stdout:
+                    print("... " + line.strip("\n"))
+            client.close()
+        return True
+    except Exception as e:
+        logging.error("Failed to sync directory {}: {}".format(local, e))
+        raise e
