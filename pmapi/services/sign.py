e84af419826c22824737ec5a2241cba557cffca9
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-17 15:28:31 -0600

    attempting to fix rpm signing

--- pmapi/services/sign.py
+++ pmapi/services/sign.py
@@ -10,43 +10,54 @@ def sign_rpm(repo, package):
     try:
         logging.info("Signing {} for repo {}".format(package, repo))
         if repo == "alpha":
-            with open("/home/mirroradmin/.rpmmacros", "w") as f:
-                f.write("% _signature gpg\n")
-                f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
-                f.write("% _gpg_name EIG Package Management\n")
-                f.write("% __gpg /bin/gpg1\n")
-            child = pexpect.spawn("rpm --addsign {}".format(package))
-            child.expect("Enter passphrase: ")
-            child.sendline(c["ALPHA_PASSPHRASE"])
+            try:
+                with open("/home/mirroradmin/.rpmmacros", "w") as f:
+                    f.write("% _signature gpg\n")
+                    f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
+                    f.write("% _gpg_name EIG Package Management\n")
+                    f.write("% __gpg /bin/gpg1\n")
+                child = pexpect.spawn("rpm --addsign {}".format(package))
+                child.expect("Enter passphrase: ")
+                child.sendline(c["ALPHA_PASSPHRASE"])
+            except Exception as e:
+                raise e
         elif repo == "beta":
-            with open("/home/mirroradmin/.rpmmacros", "w") as f:
-                f.write("% _signature gpg\n")
-                f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
-                f.write("% _gpg_name EIG Beta Signing Authority\n")
-                f.write("% __gpg /bin/gpg1\n")
-            child = pexpect.spawn("rpm --addsign {}".format(package))
-            child.expect("Enter passphrase: ")
-            child.sendline(c["BETA_PASSPHRASE"])
+            try:
+                with open("/home/mirroradmin/.rpmmacros", "w") as f:
+                    f.write("% _signature gpg\n")
+                    f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
+                    f.write("% _gpg_name EIG Beta Signing Authority\n")
+                    f.write("% __gpg /bin/gpg1\n")
+                child = pexpect.spawn("rpm --addsign {}".format(package))
+                child.expect("Enter passphrase: ")
+                child.sendline(c["BETA_PASSPHRASE"])
+            except Exception as e:
+                raise e
         elif repo == "staging":
-            with open("/home/mirroradmin/.rpmmacros", "w") as f:
-                f.write("% _signature gpg\n")
-                f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
-                f.write("% _gpg_name EIG Staging Signing Authority\n")
-                f.write("% __gpg /bin/gpg1\n")
-            child = pexpect.spawn("rpm --addsign {}".format(package))
-            child.expect("Enter passphrase: ")
-            child.sendline(c["STAGING_PASSPHRASE"])
+            try:
+                with open("/home/mirroradmin/.rpmmacros", "w") as f:
+                    f.write("% _signature gpg\n")
+                    f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
+                    f.write("% _gpg_name EIG Staging Signing Authority\n")
+                    f.write("% __gpg /bin/gpg1\n")
+                child = pexpect.spawn("rpm --addsign {}".format(package))
+                child.expect("Enter passphrase: ")
+                child.sendline(c["STAGING_PASSPHRASE"])
+            except Exception as e:
+                raise e
         elif repo == "production":
-            with open("/home/mirroradmin/.rpmmacros", "w") as f:
-                f.write("% _signature gpg\n")
-                f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
-                f.write("% _gpg_name EIG Production Signing Authority\n")
-                f.write("% __gpg /bin/gpg1\n")
-            child = pexpect.spawn("rpm --addsign {}".format(package))
-            child.expect("Enter passphrase: ")
-            child.sendline(c["PRODUCTION_PASSPHRASE"])
+            try:
+                with open("/home/mirroradmin/.rpmmacros", "w") as f:
+                    f.write("% _signature gpg\n")
+                    f.write("% _gpg_path /home/mirroradmin/.gnupg\n")
+                    f.write("% _gpg_name EIG Production Signing Authority\n")
+                    f.write("% __gpg /bin/gpg1\n")
+                child = pexpect.spawn("rpm --addsign {}".format(package))
+                child.expect("Enter passphrase: ")
+                child.sendline(c["PRODUCTION_PASSPHRASE"])
+            except Exception as e:
+                raise e
         return True
     except Exception as e:
         logging.error("Failed to sign {} for repo {}: {}".format(package, repo, e))
-        raise e
-
+        return False
