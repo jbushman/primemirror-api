9884fd7c6af5d76439aeea330b8c061aa636e3a4
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-28 07:33:59 -0600

    things

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pmapi",
-    version="0.0.34",
+    version="0.0.35",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Primemirror API",
@@ -30,6 +30,5 @@ setup(
 	"connexion",
 	"swagger-ui-bundle"
     ],
-    scripts=["pmapi/pmapi"],
     python_requires=">=3.8"
 )
